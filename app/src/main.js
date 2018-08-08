851a4426a52a50a3c4185335eb8dcf518e135bf0
Baron-zhu <553889044@qq.com>
2018-08-08 08:11:44 +0800

    第八课 vue-cli webpack 练习

--- app/src/main.js
+++ app/src/main.js
@@ -1,7 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+const Vue = require ("vue");
 import Vue from 'vue'
+const App = require ("./App.vue");
 import App from './App'
+const router = require ("./router");
 import router from './router'
 
 Vue.config.productionTip = false
